37bb7b612f84df07cd7e01191472052ec0df41d8
CalebLBaker <caleblbaker@cedarville.edu>
2018-07-21 10:32:38 -0400

    Add new line functionality to print functions

--- kernel.c
+++ kernel.c
@@ -2,7 +2,7 @@ extern char* print(const char *message);
 extern void clearScreen();
 
 void main() {
-	const char *message = "Successfully entered 64 bit kernel.";
+	const char *message = "Successfully entered 64 bit kernel.\n$";
 	clearScreen();
 	print(message);
 }
