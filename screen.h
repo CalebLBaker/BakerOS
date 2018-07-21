49e5e993132e1495b3024b3e839f616a1b73c3bc
CalebLBaker <caleblbaker@cedarville.edu>
2018-07-21 15:54:32 -0400

    Put more definitions in header files.

--- screen.h
+++ screen.h
@@ -1,18 +1,25 @@
-unsigned long long printAt(char *str, unsigned long long row, unsigned long long col);
+#ifndef SCREEN_H
+#define SCREEN_H
 
-unsigned long long printTargeted(char *src, char *dest);
+void setCursorPosition(unsigned short newPos);
 
-unsigned long long print(char *str);
+void print(char *str);
+
+void printAt(char *str, unsigned long long row, unsigned long long col);
+
+void long long printTargeted(char *src, char *dest);
+
+void scroll();
 
 unsigned long long getCursorPosition();
-
-void setCursorPosition(unsigned long long newPos);
 
 void setCursorRowCol(unsigned long long row, unsigned long long col);
 
 void printChar(char c);
 
 void printCharColor(short c);
+
+void printNewLine();
 
 unsigned long long printCharColorAt(short c, unsigned long long row, unsigned long long col);
 
@@ -23,3 +30,7 @@ void clearScreen();
 unsigned char rowFromPosition(unsigned short pos);
 
 unsigned char colFromPosition(unsigned short pos);
+
+unsigned short getCursorRowCol();
+
+#endif
---
+++ utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+
+
+void memcpy(void *src, void *dest, unsigned long long size);
+
+
+
+#endif
