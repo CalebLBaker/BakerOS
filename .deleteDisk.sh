00266ce56f8a7a84c9970e68aae4f27e6ec6f9e2
CalebLBaker <caleblbaker@cedarville.edu>
2018-08-06 21:57:43 -0400

    Suppress error messages from virtualbox

--- .deleteDisk.sh
+++ .deleteDisk.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-VBoxManage closemedium disk buttaire.vdi --delete
+echo "VBoxManage storageattach Buttaire --storagectl SATA ... --medium emptydrive ..."
+VBoxManage storageattach Buttaire --storagectl SATA --port 0 --device 0 --type hdd --medium emptydrive 2> /dev/null
+echo "VBoxManage closemedium disk buttaire.vdi --delete 2> /dev/null"
+VBoxManage closemedium disk buttaire.vdi --delete 2> /dev/null
 exit 0
 
